b159db25675d36ea9ea620708ddf22ae35c540ee
Travis Osterman <travis@osterman.co>
2021-04-24 17:12:25 -0500

    setting error handlingx

--- post.sh
+++ post.sh
@@ -7,6 +7,7 @@
 #
 
 # proxmox install on debian 10
+set -e
 echo "deb http://download.proxmox.com/debian/pve buster pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
 wget http://download.proxmox.com/debian/proxmox-ve-release-6.x.gpg -O /etc/apt/trusted.gpg.d/proxmox-ve-release-6.x.gpg
 chmod +r /etc/apt/trusted.gpg.d/proxmox-ve-release-6.x.gpg
